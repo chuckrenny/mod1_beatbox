c71f85c8c034ad9c44995dab7e1c7c97b301a695
Charles Ren <charlesren99@gmail.com>
2023-07-06 22:18:39 -0700

    Finish Iteration 1 Basics

--- lib/node.rb
+++ lib/node.rb
@@ -8,3 +8,24 @@ class Node
 
 end
 
+class LinkedList
+    attr_accessor :head
+    
+    def initialize
+      @head = nil
+    end
+    
+    def append(data)
+        new_node = Node.new(data)
+    
+        if @head.nil?
+            @head = new_node
+        else
+            current = @head
+            current = current.next_node while current.next_node
+            current.next_node = new_node
+        end
+    end
+    
+end
+    
--- spec/node_spec.rb
+++ spec/node_spec.rb
@@ -11,5 +11,20 @@ RSpec.describe Node do
       expect(list).to be_an_instance_of(LinkedList)
     end
 
-    
+    it 'can accesss attributes in Node and LinkedList' do
+      expect(node.data).to eq ('plop')
+      expect(node.next_node).to eq (nil)
+      expect(list.head).to eq(nil)
+    end
+
+    it 'can append elements to the list' do
+      list.append("doop")
+      expect(list.head.data).to eq("doop")
+      expect(list.head.next_node).to eq(nil)
+    end
+
+    it 'can count the number of nodes on the list' do
+      list.append("doop")
+      expect(list.count).to eq(1)
+    end
 end
